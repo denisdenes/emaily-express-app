af2b4c97a522d16eb1b018b74f5ed0cbaf73fdcc
Denis Denes <denis.denes@visma.com>
2018-02-06 15:39:41 +0200

    Add promises for handling user creation response

--- services/passport.js
+++ services/passport.js
@@ -13,13 +13,16 @@ passport.use(
       callbackURL: '/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleId: profile.id})
+      User.findOne({ googleId: profile.id })
         .then((existingUser) => {
           if (existingUser) {
             // we already have a record
+            done(null, existingUser);
           } else {
             // make a new record
-            new User({ googleId: profile.id}).save();
+            new User({ googleId: profile.id })
+              .save()
+              .then(user => done(null, user));
           }
         });
     }
