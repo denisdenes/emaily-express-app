21060cccc8dc6a9eda6be5b5376675ea305f7514
Denis Denes <denis.denes@visma.com>
2018-02-16 00:54:28 +0200

    Add env variables

---
+++ config/dev.js
@@ -0,0 +1,7 @@
+// dev.js - don't commit this
+module.exports = {
+  googleClientID: '496979345067-dpjvtpaotobes5nbgvb1pb175cd7294i.apps.googleusercontent.com',
+  googleClientSecret: 'eQO20UOnwOcDar4-4C7b7u4I',
+  mongoURI: 'mongodb://denis:password@ds125288.mlab.com:25288/emaily-denis-dev',
+  cookieKey: 'asdfghjikowerfghjm'
+};
