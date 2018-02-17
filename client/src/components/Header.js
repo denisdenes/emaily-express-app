1a8d1d1c4382a2add71fcb0ab81bfbea4fd38fce
Denis Denes <denis.denes@visma.com>
2018-02-18 00:01:48 +0200

    Add materialize and style the Header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,7 +4,14 @@ class Header extends Component {
   render() {
     return(
       <div>
-        Header
+        <nav>
+          <div className="nav-wrapper">
+            <a href="#" className="left brand-logo">Emaily</a>
+            <ul className="right ">
+              <li><a href="#">Login with Google</a></li>
+            </ul>
+          </div>
+        </nav>
       </div>
     );
   }
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
+import 'materialize-css/dist/css/materialize.min.css';
+import React                            from 'react';
+import ReactDOM                         from 'react-dom';
+import { Provider }                     from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import reducers from './reducers';
+import reducers                         from './reducers';
 
 import App from './components/App';
 
