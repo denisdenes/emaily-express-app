59558bc5ae3bdb376473e0d54338b7e5345a9583
Denis Denes <denis.denes@visma.com>
2018-02-17 14:00:55 +0200

    Refactor Passport method with AsyncAwait

--- services/passport.js
+++ services/passport.js
@@ -24,19 +24,18 @@ passport.use(
       callbackURL: '/auth/google/callback',
       proxy: true
     },
-    (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleId: profile.id })
-        .then((existingUser) => {
-          if (existingUser) {
-            // we already have a record
-            done(null, existingUser);
-          } else {
-            // make a new record
-            new User({ googleId: profile.id })
-              .save()
-              .then(user => done(null, user));
-          }
-        });
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id });
+
+      if (existingUser) {
+        // we already have a record
+        return done(null, existingUser);
+      }
+
+      // make a new record
+      const user = await new User({ googleId: profile.id }).save();
+      done(null, user);
+
     }
   )
 );
