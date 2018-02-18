456f0c525ebf102821cea639d8cd75ad7fd7a497
Denis Denes <denis.denes@visma.com>
2018-02-18 11:28:12 +0200

    Link tag for header logo

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect }          from 'react-redux';
+import { Link }             from 'react-router-dom';
 
 class Header extends Component {
   render() {
@@ -7,7 +8,13 @@ class Header extends Component {
       <div>
         <nav>
           <div className="nav-wrapper">
-            <a href="#" className="left brand-logo">Emaily</a>
+            <Link
+              to={this.props.auth ? '/surveys' : '/'}
+              className="left brand-logo"
+            >
+              Emaily
+            </Link>
+
             <ul className="right ">
               {this.renderContent()}
             </ul>
