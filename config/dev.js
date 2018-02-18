9a156d847699d3d0ade9a21cdbf378cd32bd83e9
Denis Denes <denis.denes@visma.com>
2018-02-18 13:28:10 +0200

    Add Stripe keys

--- config/dev.js
+++ config/dev.js
@@ -3,5 +3,7 @@ module.exports = {
   googleClientID: '496979345067-dpjvtpaotobes5nbgvb1pb175cd7294i.apps.googleusercontent.com',
   googleClientSecret: 'eQO20UOnwOcDar4-4C7b7u4I',
   mongoURI: 'mongodb://denis:password@ds125288.mlab.com:25288/emaily-denis-dev',
-  cookieKey: 'asdfghjikowerfghjm'
+  cookieKey: 'asdfghjikowerfghjm',
+  stripePublishableKey: 'pk_test_NQq7FuJvK4RAIm3sBFE0hMQS',
+  stripeSecretKey: 'sk_test_eXhjgpYTilfUVEhM6peTqyVJ'
 };
